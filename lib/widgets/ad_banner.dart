54c82a88eb347381bcd7109c9c804de62a3d347a
Monte <monte6198@gmail.com>
2023-10-24 15:33:52 +0900

    feat: 광고 배너 추가

---
+++ lib/widgets/ad_banner.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AdBanner extends StatelessWidget {
+  const AdBanner({super.key, this.adText = 'AD', this.fullWidth = false});
+
+  final bool fullWidth;
+  final String adText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: fullWidth ? MediaQuery.of(context).size.width : 350,
+      height: fullWidth ? 160 : 120,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(fullWidth ? 0 : 5)),
+        gradient: const LinearGradient(
+          colors: [
+            Color.fromRGBO(87, 32, 218, 1),
+            Color.fromRGBO(255, 25, 72, 1),
+          ],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(right: 5),
+        child: Text(
+          adText,
+          textAlign: TextAlign.end,
+          style: const TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
